78725449f9f32f16884678a57cf9f584604730ee
System Administrator <root@MacBook-Pro-de-Edgardo.local>
2021-05-30 16:57:57 -0300

    Adding truffle config

--- truffle-config.js
+++ truffle-config.js
@@ -1,23 +1,21 @@
-var HDWalletProvider = require("@truffle/hdwallet-provider");
-var mnemonic = "";
-
 module.exports = {
-  networks: {
-    development: {
-      host: "127.0.0.1",
-      port: 7545,
-      network_id: "*", // Match any network id
-    },
-    develop: {
-      port: 8545,
-    },
-    rinkeby: {
-      provider: function () {
-        return new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/v3/");
-      },
-      network_id: 4,
-      gas: 4500000,
-      gasPrice: 10000000000,
-    },
-  },
+  // Uncommenting the defaults below
+  // provides for an easier quick-start with Ganache.
+  // You can also follow this format for other networks;
+  // see <http://truffleframework.com/docs/advanced/configuration>
+  // for more details on how to specify configuration options!
+  //
+  //networks: {
+  //  development: {
+  //    host: "127.0.0.1",
+  //    port: 7545,
+  //    network_id: "*"
+  //  },
+  //  test: {
+  //    host: "127.0.0.1",
+  //    port: 7545,
+  //    network_id: "*"
+  //  }
+  //}
+  //
 };
